9dd21eb2d5ca73c0d3d6b916ca59af4029fd3c14
Tianyu Wang <terrywang@tianyu.ninja>
2018-10-19 16:15:21 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -16,15 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Mike',
-        To: 'Jen',
-        text: 'Yahoo!',
-        createdAt: new Date().getTime()
-    });
-
     socket.on('createMessage', (message) => {
         console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
