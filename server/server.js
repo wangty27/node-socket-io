0418dfe4fb9e3bd568e7993507dd9885c432668e
Tianyu Wang <terrywang@tianyu.ninja>
2018-10-19 01:58:37 -0400

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Mike',
+        To: 'Jen',
+        text: 'Yahoo!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
