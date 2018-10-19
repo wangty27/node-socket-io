43fc7933601fe05ad855bc437a104af2b7dbce44
Tianyu Wang <terrywang@tianyu.ninja>
2018-10-19 01:34:42 -0400

    Added connect and disconnect event listener

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+// listen for new connections
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+
+
+server.listen(port, () => {
     console.log('Listening on PORT 3000');    
 });
